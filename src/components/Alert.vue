ee7479d351ec2d3b0adbb1a8f0e4ee6e5166aec8
Angus Wan <work@12a.app>
2023-10-30 15:16:32 +0000

    Added alert and better top bar

---
+++ src/components/Alert.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { computed, inject } from 'vue'
+
+const regionList = inject('regionList')
+const option = inject('option')
+console.log(option.value.showHoliday)
+const showAlert = computed(() => {
+    return option.value.showHoliday && regionList.value.length > 0
+})
+</script>
+
+<template>
+    <div class="w-full px-6 py-2 not-for-print">
+        <div class="alert shadow-md" v-if="showAlert">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="stroke-info shrink-0 w-6 h-6"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+            </svg>
+            <div>
+                <div class="text-md">
+                    There are more than one region available in your country.
+                    You can select them in options.
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/* code... */
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,25 +3,52 @@
 </script>
 
 <template>
-    <footer class="mt-20 w-full h-fit bg-base-200">
-        <section class="w-full h-full grid grid-cols-1 lg:grid-cols-2 p-8 gap-8">
-            <article class="flex justify-center lg:justify-end items-center ">
+    <footer class="mt-20 w-full h-fit bg-base-200 not-for-print">
+        <section
+            class="w-full h-full grid grid-cols-1 lg:grid-cols-2 p-8 gap-8"
+        >
+            <article class="flex justify-center lg:justify-end items-center">
                 <div>
-                    This app is licensed with <a href="https://mit-license.org/" class="underline hover:text-primary active:text-accent">MIT license</a> and provided with no warranty.
+                    This app is licensed with
+                    <a
+                        href="https://mit-license.org/"
+                        class="underline hover:text-primary active:text-accent"
+                        >MIT license</a
+                    >
+                    and provided with no warranty.
                 </div>
             </article>
-            <article class="flex flex-wrap justify-center lg:justify-start items-center gap-6">
+            <article
+                class="flex flex-wrap justify-center lg:justify-start items-center gap-6"
+            >
                 <div class="lg:w-full">
-                    Source Code: <a href="https://github.com/angus6b23/GeTsu" target="_blank" class="underline hover:text-primary active:text-accent">Github</a>
+                    Source Code:
+                    <a
+                        href="https://github.com/angus6b23/GeTsu"
+                        target="_blank"
+                        class="underline hover:text-primary active:text-accent"
+                        >Github</a
+                    >
                 </div>
                 <div class="lg:w-full">
-                    Other apps: <a href="https://12a.app" target="_blank" class="underline hover:text-primary active:text-accent">12a.app</a>
+                    Other apps:
+                    <a
+                        href="https://12a.app"
+                        target="_blank"
+                        class="underline hover:text-primary active:text-accent"
+                        >12a.app</a
+                    >
                 </div>
                 <div class="lg:w-full">
-                    Support this project: <a href="https://liberapay.com/12a.app/donate" target="_blank" class="underline hover:text-primary active:text-accent">Liberpay</a>
+                    Support this project:
+                    <a
+                        href="https://liberapay.com/12a.app/donate"
+                        target="_blank"
+                        class="underline hover:text-primary active:text-accent"
+                        >Liberpay</a
+                    >
                 </div>
             </article>
-
         </section>
     </footer>
     <!-- code... -->
